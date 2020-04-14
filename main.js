ea23d957c16cb7e68f744da95e12b6ab573fa95b
DeAundre Prescott <deaundre.prescott@codeimmersives.com>
2020-04-14 09:31:57 -0400

    done

--- main.js
+++ main.js
@@ -1,7 +1,31 @@
 const Stack = function() {
+  return{
+    items: [],
+    add: function(item){
+      this.items.push(item);
+    },
+    remove: function(){
+      return this.items.pop();
+    },
+    peek: function(){
+      return this.items[this.items.length - 1];
+    },
+  }
 }
 
 const Queue = function() {
+  return{
+    items: [],
+    add: function (item){
+      return this.items.push(item);
+    },
+    remove: function(){
+      return this.items.shift();
+    },
+    peek: function(){
+      return this.items[0];
+    },
+  }
 }
 
 module.exports = {
